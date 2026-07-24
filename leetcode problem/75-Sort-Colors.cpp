a34f90c49616a563d85a685c7790318deecbc111
han-u05-cyber <hanumangupta910@gmail.com>
2026-07-24 15:32:46 +0530

    [75] [Time Beats: 100.00%] [Memory Beats: 98.19%] - LeetPush

--- leetcode problem/75-Sort-Colors.cpp
+++ leetcode problem/75-Sort-Colors.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin() , nums.end());
+        int cnt0 = 0 , cnt1 = 0 , cnt2 =0 ;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i] == 0){
+                cnt0++;
+            }
+            else if(nums[i] == 1){
+                cnt1++;
+            }
+            else{
+                cnt2++;
+            }
+        } 
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(i < cnt0){
+                nums[i] = 0;
+            }
+            else if( i < cnt0+cnt1){
+                nums[i] = 1;
+            }
+            else if(i < cnt0+cnt1+cnt2){
+                nums[i] = 2;
+            }
+        }
     }
 };
