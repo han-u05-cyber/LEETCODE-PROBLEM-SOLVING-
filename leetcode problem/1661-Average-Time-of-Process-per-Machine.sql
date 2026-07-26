53b267859b6d0588461dd20deebf51304f14c33b
han-u05-cyber <hanumangupta910@gmail.com>
2026-07-26 17:26:21 +0530

    [1661] [Time Beats: 52.58%] - LeetPush

---
+++ leetcode problem/1661-Average-Time-of-Process-per-Machine.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select b.machine_id,round(avg(b.timestamp -a.timestamp) , 3) as processing_time  from Activity a , Activity b where a.machine_id = b.machine_id AND a.process_id = b.process_id AND a.activity_type = 'start' AND b.activity_type = 'end' group by machine_id
